53a19c00f80bc02bcc5efc873268c2d167c03582
Chitranjan-VRD <priyanshusingh7839@email.com>
2026-05-28 13:07:51 +0530

    solution to problem set 0

---
+++ problem set 0/indoor.py
@@ -0,0 +1,4 @@
+# ask user for input
+activity = input("Enter text: ")
+# print the output in lower case
+print(activity.lower())
---
+++ problem set 0/playback.py
@@ -0,0 +1,4 @@
+#ask user for input 
+activity= input("Enter text: ")
+# replace the whitespace with ...
+print(activity.replace(" ", "..."))
